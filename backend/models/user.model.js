ec5a593e06791033af720b42b210911a77f9b217
Satyam Kumar <satyamkumar629301@gmail.com>
2026-04-13 12:06:14 +0530

    chnage user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -30,11 +30,11 @@ const UserSchema = new mongoose.Schema({
     },
     createdAt: {
         type:Date,
-        default:Date.now()
+        default: Date.now()
     },
     updatedAt: {
         type:Date,
-        default:Date.now()
+        default: Date.now()
     },
     token : {
         type: String,
