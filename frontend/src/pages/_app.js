5f33a047c9d3f0d0e846016b5e9db230e529150c
Satyam Kumar <satyamkumar629301@gmail.com>
2026-05-04 12:21:39 +0530

    Cleaned up the formatting on the getLayout comment

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -3,7 +3,7 @@ import '@/styles/globals.css';
 import UserLayout from '@/layout/UserLayout';
 
 export default function App({ Component, pageProps }) {
-  // 1. Check if the individual page has a custom layout (optional future proofing)
+  // Check if the individual page has a custom layout (optional future proofing)
   const getLayout = Component.getLayout || ((page) => <UserLayout>{page}</UserLayout>);
 
   return getLayout(<Component {...pageProps} />);
